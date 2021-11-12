476f5e7fd99c4425088e78d828331474b059a12b
Peter Simon <psi@estos.de>
2021-11-12 11:33:39 +0100

    configure App/Client-ID

--- src/authConfig.js
+++ src/authConfig.js
@@ -12,9 +12,9 @@ import { LogLevel } from "@azure/msal-browser";
  */
 export const msalConfig = {
     auth: {
-        clientId: "Enter_the_Application_Id_Here",
-        authority: "Enter_the_Cloud_Instance_Id_HereEnter_the_Tenant_Info_Here",
-        redirectUri: "Enter_the_Redirect_Uri_Here"
+        clientId: "cce802c7-c4e9-4d19-a128-7720788b6f78",
+        authority: "https://login.microsoftonline.com/common/",
+        redirectUri: "https://salmon-sky-06a7ffa10.azurestaticapps.net"
     },
     cache: {
         cacheLocation: "sessionStorage", // This configures where your cache will be stored
